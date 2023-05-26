c07985e289e8b109a4cffb275252bbea679e2220
hai1710 <125845377+hai1710@users.noreply.github.com>
2023-05-26 11:46:11 +0700

    update pack dao

--- src/main/java/com/codegym/dao/GameDAO.java
+++ src/main/java/com/codegym/dao/GameDAO.java
@@ -7,15 +7,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GameDAO implements IGameDAO {
-    private final String jdbcURL = "jdbc:mysql://localhost:3306/";
+    private final String jdbcURL = "jdbc:mysql://localhost:3306/quatduo?useSSL=false";
     private final String jdbcUsername = "root";
     private final String jdbcPassword = "Haido123";
 
-    private static final String INSERT_GAME_SQL = "";
-    private static final String SELECT_GAME_BY_ID = "";
-    private static final String SELECT_ALL_GAME = "";
-    private static final String UPDATE_GAME_SQL = "";
-    private static final String DETELE_GAME_SQL = "";
+    private static final String INSERT_GAME_SQL = "INSERT INTO Game (name, image_source) VALUES (?,?)";
+    private static final String SELECT_GAME_BY_ID = "SELECT id,name,image_source FROM Game WHERE id = ?";
+    private static final String SELECT_ALL_GAME = "SELECT * FROM Game";
+    private static final String UPDATE_GAME_SQL = "UPDATE Game SET name = ?, image_source = ? WHERE id = ?";
+    private static final String DETELE_GAME_SQL = "DETELE FROM Game WHERE id = ?";
 
     public GameDAO(){
     }
@@ -43,9 +43,9 @@ public class GameDAO implements IGameDAO {
             while (rs.next()){
                 int id =rs.getInt("id");
                 String name = rs.getString("name");
-                String src = rs.getString("src");
+                String image_source = rs.getString("image_source");
 
-                games.add( new Game(id,name,src));
+                games.add( new Game(id,name,image_source));
 
             }
 
@@ -56,23 +56,69 @@ public class GameDAO implements IGameDAO {
     }
 
     @Override
-    public Game getNameById(int id) {
-        return null;
+    public Game getGameById(int id) {
+        Game game = null;
+
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_GAME_BY_ID)){
+            preparedStatement.setInt(1,id);
+            System.out.println(preparedStatement);
+
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while (rs.next()){
+                String name = rs.getString("name");
+                String image_source = rs.getString("image_source");
+
+                game = new Game(id, name, image_source);
+            }
+        } catch (SQLException e){
+            printSQLException(e);
+        }
+
+        return game;
     }
 
     @Override
     public void addGame(Game game) {
+        System.out.println(INSERT_GAME_SQL);
 
+        try (Connection connection = getConnection()){
+            PreparedStatement preparedStatement = connection.prepareStatement(INSERT_GAME_SQL);
+            preparedStatement.setString(1,game.getName());
+            preparedStatement.setString(2,game.getImageSource());
+            System.out.println(preparedStatement);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e){
+            printSQLException(e);
+        }
     }
 
     @Override
-    public void updateGame(Game game) {
+    public boolean updateGame(Game game) throws SQLException{
+        boolean rowUpdated;
 
+        try(Connection connection = getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_GAME_SQL);
+            preparedStatement.setInt(1,game.getId());
+            preparedStatement.setString(2,game.getName());
+            preparedStatement.setString(3, game.getImageSource());
+
+            rowUpdated = preparedStatement.executeUpdate() > 0;
+        }
+        return rowUpdated;
     }
 
     @Override
-    public void deleteGame(int id) {
+    public boolean deleteGame(int id) throws SQLException{
+        boolean rowDeteled;
 
+        try (Connection connection = getConnection()){
+            PreparedStatement preparedStatement = connection.prepareStatement(DETELE_GAME_SQL);
+            preparedStatement.setInt(1, id);
+            rowDeteled = preparedStatement.executeUpdate() > 0;
+        }
+        return rowDeteled;
     }
     private void printSQLException(SQLException ex) {
         for (Throwable e : ex) {
